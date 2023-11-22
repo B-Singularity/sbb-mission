e51f01bcca6700885e59077b2cb0ae41583d7834
B-singularity <skil1489@naver.com>
2023-11-22 14:47:46 +0900

    chapter2-3

--- src/main/java/com/ll/sb20231114/MainController.java
+++ src/main/java/com/ll/sb20231114/MainController.java
@@ -2,11 +2,13 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class MainController {
     @GetMapping("/sbb")
-    private void index() {
-        System.out.println("index");
+    public String index() {
+        return "안녕하세요, sbb에 오신걸 환영합니다.";
     }
 }
--- src/test/java/com/ll/sb20231114/MainControllerTest.java
+++ src/test/java/com/ll/sb20231114/MainControllerTest.java
@@ -1,13 +1,14 @@
 package com.ll.sb20231114;
 
-import com.sun.tools.javac.Main;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainControllerTest {
     @Test
     public void testIndexMethod() {
         MainController mainController = new MainController();
-        assertEquals("index", mainController.index());
+        assertEquals("안녕하세요, sbb에 오신걸 환영합니다.", mainController.index());
     }
 
 }
