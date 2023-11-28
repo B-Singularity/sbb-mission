c752fde652e2ea4462790d16654dcb51370b650d
B-singularity <skil1489@naver.com>
2023-11-28 20:30:05 +0900

    방어적 복사를 이용한 테스트코드 작성

--- src/main/java/com/ll/sb20231114/Question.java
+++ src/main/java/com/ll/sb20231114/Question.java
@@ -3,6 +3,7 @@ package com.ll.sb20231114;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/ll/sb20231114/QuestionRepositoryTest.java
+++ src/test/java/com/ll/sb20231114/QuestionRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.ll.sb20231114;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class QuestionRepositoryTest {
@@ -16,7 +21,8 @@ public class QuestionRepositoryTest {
     }
 
     @Test
-    void testJpa() {
+    @DisplayName("기본 기능 테스트")
+    void test1() {
         Question q1 = Question.builder()
                 .subject("sbb가 무엇인가요?")
                 .content("sbb에 대해서 알고 싶습니다.")
@@ -30,6 +36,27 @@ public class QuestionRepositoryTest {
                 .createDate(LocalDateTime.now())
                 .build();
         this.questionRepository.save(q2);
+    }
 
+    @Test
+    @DisplayName("데이터 조회하기")
+    void test2() {
+        List<Question> originalList = this.questionRepository.findAll();
+        List<Question> copiedList = new ArrayList<>();
+
+        for (Question originalQuestion : originalList) {
+            Question copiedQuestion = Question.builder()
+                    .subject(new String(originalQuestion.getSubject()))
+                    .content(new String(originalQuestion.getContent()))
+                    .createDate(originalQuestion.getCreateDate())
+                    .build();
+
+            copiedList.add(copiedQuestion);
+        }
+
+        assertEquals(2, copiedList.size());
+
+        Question q = copiedList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
 }
